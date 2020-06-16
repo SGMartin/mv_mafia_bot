3354423c454980a4fd6e05d71e7e6c9b754b3e7f
SGMartin <s.g.martin94@gmail.com>
2020-06-16 16:26:34 +0200

    Added more complex vote routines

--- game_day.py
+++ game_day.py
@@ -3,21 +3,28 @@ import pandas as pd
 import requests
 
 class  GameDay:
-    def __init__(self, post_id: int, day_number:int, post: requests.get):
+    def __init__(self, post_id: int, day_number:int, day_start_post: requests.get):
 
-        self.post_id      = post_id
-        self.day_number   = day_number
-        self.players      = self.get_player_list(post)
+        self.post_id            = post_id
+        self.day_number         = day_number
+        self.majority_reached   = False
+
+
+        # Load vote rights table
+        self.vote_rights = pd.read_csv('vote_config.csv', sep=',')
+        self.vote_rights.set_index('player', inplace=True)
+
+        self.alive_players      = self.get_player_list(day_start_post)
 
         # When players are even, majority is players / 2 + 1
-        # When players are odd,  majority is players  / 2 rounded up
-        
-        if (len(self.players) % 2) == 0:
-            self.majority = int(len(self.players) / 2) + 1
+        # When players are odd,  majority is players  / 2 rounded up  
+        if (len(self.alive_players) % 2) == 0:
+            self.majority = int(len(self.alive_players) / 2) + 1
         else:
-            self.majority = int(round(len(self.players)/2, 0))
+            self.majority = int(round(len(self.alive_players)/2, 0))
         
-        
+        # Create vote count table
+        self.vote_table = pd.DataFrame(columns=['player', 'voted_by', 'post_id'])    
 
     def get_player_list(self, parser) -> list():
         '''
@@ -46,21 +53,81 @@ class  GameDay:
 
 
 
-    def vote_player(self, victim:str, player:str, post:int):
+    def is_valid_vote(self, player:str, victim:str) -> bool:
         '''
-        Keeps track of votes.
-        
-        if  'desvoto' in victim.lower(): #This is easier for pple using dots after vote.
-            print(player, 'is unvoting.')
-        
-        if  'no linchamiento' in victim.lower():
-            print(player, 'chose not to lynch!')
-        
-        if victim in self.players: ## Oho, it's a valid victim
-
-
-        
-
-
-        print(f'{player} voted {victim} at {post}')
+        Check if a casted vote is legit
         '''
+        self._is_valid_vote = False
+
+        if not self.majority_reached:
+
+            if player in self.alive_players:
+
+                self._player_max_votes = self.vote_rights.loc[player, 'allowed_votes']
+                self._player_current_votes = len(self.vote_table[self.vote_table['voted_by'] == player])
+            
+                if victim.lower() in 'desvoto' and self._player_current_votes > 0:
+                    self._is_valid_vote = True
+            
+                if victim.lower() in 'no linchamiento' and self._player_current_votes < self._player_max_votes:
+                    self._is_valid_vote  = True
+            
+                if victim in self.alive_players:
+                    if self.vote_rights.loc[victim, 'can_be_voted'] == 1:
+                        if self._player_current_votes < self._player_max_votes:
+                            self._is_valid_vote = True
+    
+        return self._is_valid_vote
+
+                
+
+    def is_lynched(self, victim:str) -> bool:
+        '''
+        Check if this player has reached lynch majority to be lynched
+        '''
+        self._lynched = False
+
+        # Count this player votes
+        self._lynch_votes = len(self.vote_table[self.vote_table['player'] == victim])
+        self._player_majority = self.majority + self.vote_rights.loc[victim, 'mod_to_lynch']
+
+        if self._lynch_votes >= self._player_majority:
+            self._lynched = True
+        
+        return self._lynched
+
+
+
+    def vote_player(self, player:str, victim:str, post_id:int):
+
+        if self.is_valid_vote(player, victim):
+
+            if victim.lower() in  'desvoto':
+                self._old_vote = self.vote_table[self.vote_table['voted_by'] == player].index[0]
+                self.vote_table.drop(self._old_vote, axis=0, inplace=True)
+                print(player, 'unvoted.')
+            
+            else:
+                self._victim = victim
+
+                if victim.lower() in 'no linchamiento':
+                    self._victim = 'no_lynch'
+
+                self.vote_table  = self.vote_table.append({'player': self._victim,
+                                                           'voted_by': player,
+                                                           'post_id': post_id},
+                                                           ignore_index=True)
+                
+                print(player, 'voted', self._victim)
+
+                #Check if we have reached majority
+                if self.is_lynched(self._victim):
+                    self.majority_reached = True        
+
+        else:
+            print('Invalid vote by', player, 'at', post_id, 'They voted', victim)
+    
+
+
+           
+
