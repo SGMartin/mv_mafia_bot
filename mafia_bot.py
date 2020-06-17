57750d4c4b541e46bbe58926bb0322f37e624b0d
SGMartin <s.g.martin94@gmail.com>
2020-06-17 19:31:55 +0200

    Votecounts are finally working

--- mafia_bot.py
+++ mafia_bot.py
@@ -3,7 +3,7 @@ import time
 import re
 
 from bs4 import BeautifulSoup
-
+import pandas as pd
 
 class MafiaBot:
 
@@ -16,8 +16,18 @@ class MafiaBot:
         self.game_master           = game_master
 
         self._current_day_start_post  = 1
+        self.player_list              = []
 
-        self.run(10)
+        # Load vote rights table
+        self.vote_rights = pd.read_csv('vote_config.csv', sep=',')
+        self.vote_rights.set_index('player', inplace=True)
+
+        # Initialize empty vote table
+        self.vote_table = pd.DataFrame(columns=['player', 'voted_by', 'post_id'])
+
+        self.majority_reached         = False
+
+        self.run(loop_waittime_seconds)
     
     def run(self, update_tick:int):
 
@@ -82,10 +92,12 @@ class MafiaBot:
                     if self._phase_end:
                         return self._is_day_phase
                       
-                    elif self._phase_start:
+                    elif self._phase_start: 
 
+                        #TODO: We should start thinking about an standalone method here
                         self._current_day_start_post = int(self._post['data-num'])
                         self._is_day_phase = True
+                        self.player_list = self.get_player_list(self._current_day_start_post)
 
         return self._is_day_phase
 
@@ -129,8 +141,8 @@ class MafiaBot:
                         elif 'voto' in self._bolded_paragraph.text.lower():
                             self._victim = self._bolded_paragraph.text.split(' ')[-1]
                     
-            if self._victim != '': # Call votecount routine here
-                print(self._author, 'voted:', self._victim)
+                        if self._victim != '': # Call votecount routine here
+                            self.vote_player(self._author, self._victim, self._post_id)
 
 
 
@@ -169,3 +181,120 @@ class MafiaBot:
 
         self._page_number = int(round(post_id / 30))
         return self._page_number
+
+
+    def get_player_list(self, start_day_post_id:int) -> list():
+        '''
+        Parses opening day post to get a list of active players. We are sent
+        an http get request with the day post. This way, we can recycle a previous
+        request.
+        '''
+        self._start_day_page = self.get_page_number_from_post(start_day_post_id)
+
+        self._request_url = f'{self.game_thread}/{self._start_day_page}'
+        self._request     = requests.get(self._request_url).text
+
+        self._response  = BeautifulSoup(self._request, 'html.parser')
+        self._all_posts = self._response.find_all('div', attrs={'data-num':True,
+                                                                'data-autor':True})
+        
+        self._player_list = []
+
+        for self._post in self._all_posts:
+
+            ##We found the post of interest
+            if int(self._post['data-num']) == start_day_post_id: 
+
+                # Get the first list. It should be the player lists according to the templace
+                self._players = self._post.find('ol').find_all('a')
+
+                for self._player in self._players:
+                    self._player_list.append(self._player.contents[0])
+
+                return self._player_list
+
+
+    def is_valid_vote(self, player:str, victim:str) -> bool:
+
+        self._is_valid_vote = False
+
+        if not self.majority_reached:
+
+            if player in self.player_list or player == self.game_master:
+
+                if player == self.game_master:
+                    self._player_max_votes = 999
+                else:
+                    self._player_max_votes = self.vote_rights.loc[player, 'allowed_votes']
+
+                self._player_current_votes = len(self.vote_table[self.vote_table['voted_by'] == player])
+            
+                if victim == 'desvoto' and self._player_current_votes > 0:
+                    self._is_valid_vote = True
+            
+                elif victim  == 'no_lynch' and self._player_current_votes < self._player_max_votes:
+                    self._is_valid_vote  = True
+            
+                elif victim in self.player_list:
+
+                    if victim in self.vote_rights.index:
+                        if self.vote_rights.loc[victim, 'can_be_voted'] == 1:
+                            if self._player_current_votes < self._player_max_votes:
+                                self._is_valid_vote = True
+                    else:
+                        print('Warning: player', victim, 'is not on the vote rights table.')
+    
+        return self._is_valid_vote
+
+    
+    def is_lynched(self, victim:str) -> bool:
+        '''
+        Check if this player has reached lynch majority to be lynched
+        '''
+        self._lynched = False
+
+        # Count this player votes
+        self._lynch_votes = len(self.vote_table[self.vote_table['player'] == victim])
+        self._player_majority = self.get_vote_majority() + self.vote_rights.loc[victim, 'mod_to_lynch']
+
+        if self._lynch_votes >= self._player_majority:
+            self._lynched = True
+        
+        return self._lynched
+
+    
+    def vote_player(self, player:str, victim:str, post_id:int):
+
+        if self.is_valid_vote(player, victim):
+
+            if victim == 'desvoto':
+                self._old_vote = self.vote_table[self.vote_table['voted_by'] == player].index[0]
+                self.vote_table.drop(self._old_vote, axis=0, inplace=True)
+                print(player, 'unvoted.')
+            
+            else:
+                self.vote_table  = self.vote_table.append({'player': victim,
+                                                           'voted_by': player,
+                                                           'post_id': post_id},
+                                                           ignore_index=True)
+                
+                print(player, 'voted', self._victim)
+
+                #Check if we have reached majority
+                if self.is_lynched(self._victim):
+                    self.majority_reached = True    
+        else:
+            print('Invalid vote by', player, 'at', post_id, 'They voted', self._victim)
+
+
+    def get_vote_majority(self) -> int:
+
+        # When players are even, majority is players / 2 + 1
+        # When players are odd,  majority is players  / 2 rounded up  
+        
+        if (len(self.player_list) % 2) == 0:
+            self._majority = int(len(self.player_list) / 2) + 1
+        else:
+            self._majority = int(round(len(self.player_list)/2, 0))
+
+        return self._majority
