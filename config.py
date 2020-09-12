b3947697792b6edfcbea9dc2645ba0ce2853f512
SGMartin <s.g.martin94@gmail.com>
2020-09-12 11:02:00 +0200

    Stub for a config class

---
+++ config.py
@@ -0,0 +1,62 @@
+import logging
+import os.path
+import sys
+
+import pandas as pd
+
+
+class Config:
+
+    def __init__(self, file_to_load):
+        '''
+        stub
+        '''
+        self.game_thread        = ''
+        self.game_master        = ''
+        self.mediavida_user     = ''
+        self.mediavida_pwd      = ''
+        self.update_time        = 60
+        self.posts_until_update = 30
+
+        self._raw_config  = self._load_file(file_to_load)
+
+        self._parse_config(self._raw_config)
+
+
+    def _load_file(self, file_to_load:str) -> pd.DataFrame:
+        '''
+        Loads a config file.
+        '''
+        if os.path.isfile(file_to_load):
+            try:
+                self._config = pd.read_csv(file_to_load, sep=',', index_col=0)
+                return self._config
+            except:
+                logging.critical('Could not load config file. Check the format.')
+                sys.exit('Could not load config file.')
+        else:
+            logging.critical('Cannot find config file.')
+            sys.exit('Could not find config file.')
+
+    
+    def _parse_config(self, raw_config: pd.DataFrame) -> dict:
+        '''
+        '''
+        self.game_thread    = raw_config.loc['game_thread', 'value'] 
+        self.game_master    = raw_config.loc['game_master', 'value']
+        
+        self.mediavida_user = raw_config.loc['mediavida_user', 'value']
+        self.mediavida_pwd  = raw_config.loc['mediavida_password', 'value']
+
+        self.update_time    = raw_config.loc['update_time_seconds', 'value']
+        self.posts_until_update = raw_config.loc['push_vote_count_interval', 'value']
+
+
+        if self.update_time < 10:
+            self.update_time == 10
+            
+        if self.posts_until_update <= 0:
+            self.posts_until_update = -1
+
+
+
