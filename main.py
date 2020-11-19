c4226ff79dba55d12c3c3367462375a4233f5436
Santiago Garcia <s.g.martin94@gmail.com>
2020-11-19 18:42:31 +0100

    Redirected config fetch to config class

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 import os.path
 import pandas as pd
 
+import config
 import mafia_bot
 import user
 
@@ -23,41 +24,16 @@ def main():
     logging.getLogger("requests").setLevel(logging.WARNING)
     logging.getLogger("urllib3").setLevel(logging.WARNING)
 
-    # Load confic  dict.
-    bot_config = {'game_thread': '',
-                  'gm': '',
-                  'mv_id': '',
-                  'mv_password': ''}
+    Config = config.Config(file_to_load='config.csv')
 
-    ## Attempt to load config file ##
-    try:
-        config = pd.read_csv('config.csv', sep=',', index_col=0)
-        bot_config['game_thread'] = config.loc['game_thread', 'value']
-        bot_config['gm']          = config.loc['GM', 'value']
-        bot_config['mv_id']       = config.loc['mediavida_user', 'value']
-        bot_config['mv_password'] = config.loc['mediavida_password', 'value']
-        bot_config['update_time_seconds'] = config.loc['update_time_seconds', 'value']
-        bot_config['post_push_interval']  = config.loc['push_vote_count_interval', 'value']
-
-        print('Configuration... LOADED')
-        logging.info('Configuration loaded')
-    
-    except:
-        logging.critical('Failed to load config.csv')
-        raise
-    
-
-
-    MafiaBot = mafia_bot.MafiaBot(game_url=bot_config['game_thread'],
-                                  game_master=bot_config['gm'],
-                                  bot_userID=bot_config['mv_id'],
-                                  bot_password=bot_config['mv_password'],
-                                  loop_waittime_seconds=int(bot_config['update_time_seconds']),
-                                  post_push_interval = int(bot_config['post_push_interval'])
+    MafiaBot = mafia_bot.MafiaBot(game_url=Config.game_thread,
+                                  game_master=Config.game_master,
+                                  bot_userID=Config.mediavida_user,
+                                  bot_password=Config.mediavida_pwd,
+                                  loop_waittime_seconds=Config.update_time,
+                                  post_push_interval = Config.posts_until_update)
                                  )
     
 
-
- 
 if __name__ == "__main__":
 	main()
