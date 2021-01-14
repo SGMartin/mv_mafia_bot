0f2862f3981c049c36cc63181cbd35bd5ec40628
SGMartin <s.g.martin94@gmail.com>
2021-01-14 12:25:34 +0100

    Fix syntax error caused by PR #17
    Added a missing comma in the action responses dict.

--- modules/game_actions.py
+++ modules/game_actions.py
@@ -21,7 +21,7 @@ class GameAction:
                                    actions.Action.request_count: self._parse_vote_count_request,
                                    actions.Action.vote_history: self._request_vote_history,
                                    actions.Action.get_voters: self._request_voters_history,
-                                   actions.Action.modkill: self._modkill
+                                   actions.Action.modkill: self._modkill,
                                    actions.Action.freeze_vote: self._freeze_votes}
 
         # Parse command type
