f3c0ea873fa43884911d6e5b2e2f76c19ee90406
Santiago Garcia <s.g.martin94@gmail.com>
2021-01-23 14:53:58 +0100

    Add  docstrings for the user class

--- user.py
+++ user.py
@@ -8,7 +8,7 @@ from robobrowser import RoboBrowser
 import pandas as pd
 
 import modules.thread_reader as tr
-
+import modules.game_actions  
 
 class User:
 
@@ -34,10 +34,19 @@ class User:
        
 
     def clear_queue(self):
+        """Empty the queue of messages to push to the game thread."""
         self._queue.clear()
 
 
-    def add_vhistory_to_queue(self, action, vhistory, victim_is_voter):
+    def add_vhistory_to_queue(self, action:modules.game_actions.GameAction, vhistory:pd.DataFrame, victim_is_voter:bool):
+        """Generate a vhistory message and append it to the queue.
+
+        Args:
+            action (modules.game_actions.GameAction): Vhistory request action.
+            vhistory (pd.DataFrame): The whole vote history as a pandas dataframe.
+            victim_is_voter (bool): If the action victim is the voter or the voted player.
+        """
+
 
         self._message = self.generate_history_message(vhistory=vhistory,
                                                       is_voter=victim_is_voter,
@@ -48,6 +57,7 @@ class User:
 
 
     def push_queue(self):
+        """Post the whole queue of messages to the game thread."""
 
         if len(self._queue) > 0:
             self._resolved_queue = '\n'.join(self._queue)
@@ -55,8 +65,16 @@ class User:
             self.clear_queue()
 
 
-    def push_votecount(self, vote_count, alive_players, vote_majority, post_id):
+    def push_votecount(self, vote_count:pd.DataFrame, alive_players:int, vote_majority:int, post_id:int):
+        """Generate a new vote count message and push it to the game thread. Skips the queue.
 
+        Args:
+            vote_count (pd.DataFrame): The vote count object used to generate the message.
+            alive_players (int): The number of alive players
+            vote_majority (int): The number of votes necessary to reach majority.
+            post_id (int): The post number of the last vote.
+        """
+        
         self._message_to_post = self.generate_vote_message(vote_count=vote_count,
                                                            alive_players=alive_players,
                                                            vote_majority=vote_majority,
@@ -64,7 +82,14 @@ class User:
         self.post(self._message_to_post)
 
     
-    def push_lynch(self, last_votecount, victim, post_id):
+    def push_lynch(self, last_votecount: pd.DataFrame, victim:str, post_id:int):
+        """Generate a player lynched message and immediately post it the game thread. Skips the queue.
+
+        Args:
+            last_votecount (pd.DataFrame): The vote count table after the last vote.
+            victim (str): The lynched player name.
+            post_id (int): The post number where the vote triggering the lynch was casted.
+        """
 
         self._message_to_post = self.generate_lynch_message(last_votecount=last_votecount,
                                                             victim=victim,
@@ -72,7 +97,16 @@ class User:
         self.post(self._message_to_post)
 
 
-    def login(self, user, password):
+    def login(self, user:str, password:str):
+        """Open and resolve mediavida.com login form to log into the bot account.
+
+        Args:
+            user (str): The user id to log into the account.
+            password (str): The password to log into the account.
+
+        Returns:
+            [Robobrowser]: The resolved form.
+        """
 
         self._browser = RoboBrowser(parser="html.parser")
         self._browser.open('http://m.mediavida.com/login')
@@ -86,7 +120,15 @@ class User:
         return self._browser
 
 
-    def post(self, message):
+    def post(self, message:str):
+        """Open and resolve the post message form from mediavida.com
+
+        Args:
+            message (str): The message to post in the game thread.
+
+        Returns:
+            [Robobrowser]: The resolved form.
+        """
         self.browser.open(f'http://www.mediavida.com/foro/post.php?tid={self.thread_id}')
         self._post  = self.browser.get_form(id='postear')
         self._post['cuerpo'].value = message
@@ -95,7 +137,19 @@ class User:
         
         return self.browser.url
 
+
     def generate_vote_message(self, vote_count: pd.DataFrame, alive_players:int, vote_majority:int, post_id:int):
+        """Generate a formatted Markdown message representing the vote count results.
+
+        Args:
+            vote_count (pd.DataFrame): The vote count to parse.
+            alive_players (int): The number of alive players.
+            vote_majority (int): The current number of votes to reach abs.majority.
+            post_id (int): The post id of the last vote parsed in the vote_count.
+
+        Returns:
+            [str]: A string formatted in Markdown suitable to be posted as a new message in mediavida.com
+        """
 
         self._header = "# Recuento de votos \n"
 
@@ -110,7 +164,45 @@ class User:
         return self._message
 
 
+    def generate_lynch_message(self, last_votecount: pd.DataFrame, victim:str, post_id:int):
+        """Generate a formatted Markdown message announcing a player lynch.
+
+        Args:
+            last_votecount (pd.DataFrame): The vote count when the player is lynched.
+            victim (str): The lynched player.
+            post_id (int): The post id of the last vote before the lynch.
+
+        Returns:
+            [str]: A formatted Markdown message.
+        """
+
+        self._header = '# Recuento de votos final \n'
+
+        if victim  == 'no_lynch':
+            self._announcement = f'### ¡Se ha alcanzado mayoría absoluta en {post_id}. Nadie será linchado! ### \n'
+        
+        else:
+            self._announcement = f'### ¡Se ha alcanzado mayoría absoluta en {post_id}, se linchará a {victim}! ### \n'
+
+        self._final_votecount = self.generate_string_from_vote_count(vote_table=last_votecount)
+
+        self._no_votes = f'**Ya no se admiten más votos.** \n \n'
+        self._footer   = f'@{self.game_master}, el pueblo ha hablado. \n'
+
+        self._message = self._header + self._final_votecount + '\n' + self._announcement + self._no_votes + self._footer
+
+        return self._message
+
+
     def generate_string_from_vote_count(self, vote_table: pd.DataFrame):
+        """Generate a formatted Markdown message representing the results from the current vote count.
+
+        Args:
+            vote_table (pd.DataFrame): A pandas dataframe with the current vote count.
+
+        Returns:
+            [str]: A string formatted in Markdown table suited to be posted in mediavida.com
+        """
 
         self._vote_table = vote_table
 
@@ -134,27 +226,18 @@ class User:
         return self._vote_rank
 
 
-    def generate_lynch_message(self, last_votecount: pd.DataFrame, victim:str, post_id:int):
-
-        self._header = '# Recuento de votos final \n'
-
-        if victim  == 'no_lynch':
-            self._announcement = f'### ¡Se ha alcanzado mayoría absoluta en {post_id}. Nadie será linchado! ### \n'
-        
-        else:
-            self._announcement = f'### ¡Se ha alcanzado mayoría absoluta en {post_id}, se linchará a {victim}! ### \n'
-
-        self._final_votecount = self.generate_string_from_vote_count(vote_table=last_votecount)
-
-        self._no_votes = f'**Ya no se admiten más votos.** \n \n'
-        self._footer   = f'@{self.game_master}, el pueblo ha hablado. \n'
-
-        self._message = self._header + self._final_votecount + '\n' + self._announcement + self._no_votes + self._footer
-
-        return self._message
-
-
     def generate_history_message(self, vhistory:pd.DataFrame, is_voter:bool, player:str, requested_by:str):
+        """Generate a vote history report as a Markdown formatted string to be posted in mediavida.com
+
+        Args:
+            vhistory (pd.DataFrame): The current history of votes from the start of the game.
+            is_voter (bool): If the report is from a player casted votes or the votes casted to the player.
+            player (str): The player from which to generate the report.
+            requested_by (str): The player requesting the report.
+
+        Returns:
+            [str]: A string formatted in Markdown suited to be posted in mediavida.com
+        """
 
         #TODO: Consider an enumerator in the future
         if is_voter:
